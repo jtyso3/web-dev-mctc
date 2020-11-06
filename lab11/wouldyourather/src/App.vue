9cb756a08e664378ed719d3bd6eca10b72bcc5df
jtyso3 <57105284+jtyso3@users.noreply.github.com>
2020-11-06 17:19:32 -0600

    This is Lab 11 part one. This was a simple program to learn Vue.js. This program uses  App.vue and WouldYouRather.vue to create this webpage. In this lab I learned how each file works in a parent-child relationship to make the webpage more dynamic by using various methods to exchange data. These methods include passing data from parent -> child, and, child -> parent, in-order to dynamically display data based on the Users choice when selecting radio input

---
+++ lab11/wouldyourather/src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+     <h1> Would You Rather...</h1>
+     <would-you-rather v-bind:question="wyrQuestion" 
+     v-bind:answer1="wyrAnswer1"
+     v-bind:answer2="wyrAnswer2"
+     v-on:answer-changed='answerChanged'    
+
+     ></would-you-rather>
+
+     <p> {{ userSelectedMessage }}</p>
+
+
+  </div>
+</template>
+
+<script>
+import WouldYouRather from './components/WouldYouRather.vue'
+
+export default {
+  name: 'App',
+  components: {
+    WouldYouRather
+  },
+  data(){
+    return{
+    wyrQuestion: 'Would you rather be a wizard or a superhero?',
+    wyrAnswer1: 'Wizard',
+    wyrAnswer2: 'Superhero',
+    userSelectedMessage: ''
+    }
+  },
+  methods:{
+    answerChanged(choice){
+      this.userSelectedMessage = `Thanks! You chose ${choice}.`
+    }
+  }
+}
+</script>
+
+<style>
+body{
+  background: lightcoral;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #f5f115;
+  background: rgb(0, 0, 0);
+  margin-top: 60px;
+  border-radius: 70px;
+}
+</style>
---
+++ lab11/wouldyourather/src/components/WouldYouRather.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="wyr">
+    <h2>Please Make Your Choice</h2>
+    <h3>{{ question }}</h3>
+    
+    <div>
+      <label for="wizad"> {{ answer1 }}</label>
+      <input type="radio" name="wizard" v-model ="choice" v-bind:value="answer1" v-on:change="changeMade">
+
+      <label for="superhero">{{ answer2 }}</label>
+      <input type="radio" name="superhero" v-model="choice" v-bind:value="answer2" v-on:change="changeMade">
+    </div>
+
+  </div>
+    
+</template>
+
+<script>
+export default {
+  name: 'WouldYouRather',
+  props: {    //  binds data form app.vue to use. 
+    question: String,
+    answer1: String,
+    answer2: String
+  },
+  data(){
+    return{
+      choice: ''   // uses v-model to make radio button work. value of select = choice. 
+    }
+  },
+  methods:{
+    changeMade(){
+      return this.$emit('answer-changed', this.choice)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 20px 20px 20px;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
